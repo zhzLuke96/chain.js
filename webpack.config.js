0f802841624039b9d5b6063ec03ecd83035981f3
sanshi <zhz961101@gmail.com>
2019-04-22 18:23:31 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+// const htmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    devtool: "cheap-module-eval-source-map",
+    // entry: ["@babel/polyfill","./src/main.js"],
+    entry: {
+        "chain.common":"./src/index.js",
+        "chain": "./src/index.common.js"
+    },
+    output: {
+        path: path.join(__dirname, "./dist/"),
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: [".es6", ".js", ".jsx", ".tsx", ".ts"]
+    },
+    devServer: {
+        contentBase: "./dist"
+    },
+    plugins: [
+        // new htmlWebpackPlugin({
+        //     template: "./index.html"
+        // }),
+        new CleanWebpackPlugin({
+            verbose: true,
+            dry: false
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    }
+};
