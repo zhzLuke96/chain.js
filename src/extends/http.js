668b2add447fb4848b90ac89c2376b0d2dbdfba3
sanshi <zhz961101@gmail.com>
2019-04-24 20:32:06 +0800

    http callback fixed

--- src/extends/http.js
+++ src/extends/http.js
@@ -1,46 +1,30 @@
-function GetXhr(){
-    var xhr=null;
-    if(XMLHttpRequest){
-        xhr=new XMLHttpRequest();
-    }else{
-        xhr=new ActiveXObject("Microsoft.XMLHTTP");
+function GetXhr() {
+    var xhr = null;
+    if (XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
     return xhr;
 }
 
-export default{
-    get(cb){
-        return function(url,resolve){
-            if(cb){
-                this.__ref.opts.push(
-                    function(ctx,resolve){
-                        let res = cb.call(this,ctx,resolve);
-                        if(res)this.ctx = res
-                        resolve()
-                    }
-                )
-            }
+export default {
+    get(cb) {
+        cb = cb || (v => v)
+        return function (url, resolve) {
             const xhr = GetXhr();
             xhr.open("GET", url);
-            xhr.onload = () => resolve(false, xhr.responseText);
+            xhr.onload = () => resolve(true, cb(xhr.responseText));
             xhr.onerror = () => this.__ref.crash(this, xhr.statusText)
             xhr.send(null);
         }
     },
-    post(data,cb){
-        return function(url,resolve){
-            if(cb){
-                this.__ref.opts.push(
-                    function(ctx,resolve){
-                        let res = cb.call(this,ctx,resolve);
-                        if(res)this.ctx = res
-                        resolve()
-                    }
-                )
-            }
+    post(data, cb) {
+        cb = cb || (v => v)
+        return function (url, resolve) {
             const xhr = GetXhr();
             xhr.open("POST", url);
-            xhr.onload = () => resolve(false, xhr.responseText);
+            xhr.onload = () => resolve(true, cb(xhr.responseText));
             xhr.onerror = () => this.__ref.crash(this, xhr.statusText);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset-UTF-8");
             xhr.send(data);
